6e3e2ace8048770ccd145033bf5e8c112aa2a87c
Ilia Valchenko <ilia.valchenko.epam@gmail.com>
2016-07-03 15:51:49 +0300

    Add tasks for Day 2

---
+++ EuclidianMath/EuclidianMath/EuclidianMath.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidianMath
+{
+    public static class EuclidianMath
+    {
+        public static int GCD(int a, int b)
+        {
+            int helper;
+
+            while (b != 0)
+            {
+                helper = b;
+                b = a % b;
+                a = helper;
+            }
+            return a;
+        }
+
+        public static int GCD(int a, int b, int c)
+        {
+            int d = GCD(a, b);
+            return GCD(d, c);
+        }
+
+        public static int GCD(params int[] arr)
+        {
+            int res = 0;
+
+            if (arr.Length >= 2)
+            {
+                if (arr.Length == 2)
+                    return GCD(arr[0], arr[1]);
+
+                res = GCD(arr[0], arr[1]);
+
+                for (int i = 2; i < arr.Length; i++)
+                {
+                    res = GCD(res, arr[i]);
+                }
+            }
+            else
+                throw new ArgumentException("The number of parameters must be greater than one.");
+
+            return res;
+        }
+    }
+}
---
+++ NewtonMethod/NewtonMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num = 15;
+            int power = 8;
+            double e = 0.0000015;
+            double firstAproach = num / power;
+
+            Console.WriteLine("Number: {0}, Root power: {1}, Accuracy: {2}, First aproach: {3}", num, power, e, firstAproach);
+            Console.WriteLine("Newton method: {0}", NewtonMethod(num, power, e, firstAproach));
+            Console.WriteLine("Math.Pow: {0}", Math.Pow(num, 1/power));
+
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+
+        static double NewtonMethod(int number, int power, double accuracy, double firstAproach)
+        {
+            double x = firstAproach;
+
+            while(true)
+            {
+                double f = Math.Pow(x, power) - number;
+
+                if (Math.Abs(f) < accuracy) 
+                    break;
+
+                x += -f / (power * Math.Pow(x, power - 1));
+            }
+
+            return x;
+        }
+    }
+}
